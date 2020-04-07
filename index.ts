c7c43c58661b40641a5a1b98262735fa3ddd86e1
Federico Brigante <opensource@bfred.it>
2020-04-07 02:50:57 +0200

    Avoid errors when running in non-browser environments /2

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 const isExtensionContext = typeof chrome === 'object' && chrome && typeof chrome.extension === 'object';
-const globalWindow = typeof window === 'object' && window;
+const globalWindow = typeof window === 'object' ? window : undefined;
 const isWeb = location.protocol.startsWith('http');
 
 export function isContentScript(): boolean {
