d51ce6d232ed1c6715e8f9d0ad5e52992ac76ddf
Federico <me@fregante.com>
2021-06-27 20:16:09 +0700

    Use `globalThis` to simplify code

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-const globalWindow = typeof window === 'object' ? window : undefined;
-
 let cache = true;
 export function disableWebextDetectPageCache(): void {
 	cache = false;
@@ -17,27 +15,27 @@ function once(function_: () => boolean): () => boolean {
 }
 
 /** Indicates whether the code is being run on http(s):// pages (it could be in a content script or regular web context) */
-export const isWebPage = once((): boolean => {
-	return typeof location === 'object' && location.protocol.startsWith('http');
-});
+export const isWebPage = once((): boolean =>
+	globalThis.location?.protocol.startsWith('http')
+);
 
 /** Indicates whether the code is being run in extension contexts that have access to the chrome API */
-export const isExtensionContext = once((): boolean => {
-	return typeof chrome === 'object' && chrome && typeof chrome.extension === 'object';
-});
+export const isExtensionContext = once((): boolean =>
+	typeof globalThis.chrome?.extension === 'object'
+);
 
 /** Indicates whether the code is being run in a content script */
-export const isContentScript = once((): boolean => {
-	return isExtensionContext() && isWebPage();
-});
+export const isContentScript = once((): boolean =>
+	isExtensionContext() && isWebPage()
+);
 
 /** Indicates whether the code is being run in a background page */
-export const isBackgroundPage = once((): boolean => {
-	return isExtensionContext() && (
+export const isBackgroundPage = once((): boolean =>
+	isExtensionContext() && (
 		location.pathname === '/_generated_background_page.html' ||
-		chrome.extension?.getBackgroundPage?.() === globalWindow
-	);
-});
+		chrome.extension?.getBackgroundPage?.() === globalThis.window
+	)
+);
 
 /** Indicates whether the code is being run in an options page. This only works if the current page’s URL matches the one specified in the extension's `manifest.json` */
 export const isOptionsPage = once((): boolean => {
@@ -46,12 +44,10 @@ export const isOptionsPage = once((): boolean => {
 	}
 
 	const {options_ui} = chrome.runtime.getManifest();
-	if (typeof options_ui !== 'object' || typeof options_ui.page !== 'string') {
+	if (options_ui?.page !== 'string') {
 		return false;
 	}
 
 	const url = new URL(options_ui.page, location.origin);
-
-	return url.pathname === location.pathname &&
-		url.origin === location.origin;
+	return url.pathname === location.pathname;
 });
